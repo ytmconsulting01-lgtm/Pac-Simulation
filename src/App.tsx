36288bc78d3766a5209be9b64cd4186da7b90816
ytmconsulting01-lgtm <ytmconsulting01@gmail.com>
2026-02-23 17:23:14 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 
 type ProfilAides = "Grand précaire" | "Modeste" | "Violet" | "Classique";
 type ChauffageActuel = "Gaz" | "Fioul" | "Électricité" | "Poêle à granulés" | "Autre";
